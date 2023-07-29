8ae88bb93ee263d6928cae80e3e16e3f37d54b8a
Rob Johnson <johnson.rob.alan@gmail.com>
2023-07-28 20:31:02 -0700

    timer interrupt to update the app and move the ball.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
 
 
 class PongBall(Widget):
@@ -14,12 +15,23 @@ class PongBall(Widget):
 
 
 class PongGame(Widget):
-    pass
+    ball = ObjectProperty(None)
+
+    def update(self, dt):
+        self.ball.move()
+
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
 
 
 class PongApp(App):
     def build(self):
-        return PongGame()
+        game = PongGame()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
 
 
 if __name__ == "__main__":
